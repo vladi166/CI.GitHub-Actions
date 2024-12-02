326f59a8155930d1ce54038731c0be00c5e31f90
Vladislav Andreev <vladireft96@mail.ru>
2024-12-02 13:18:24 +0500

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/CI/GitHubActions/StatisticsService.java
@@ -0,0 +1,13 @@
+package ru.netology.javaqa.CI.GitHubActions;
+
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
